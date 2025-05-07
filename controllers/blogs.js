e3ff8e8ff6e129a7ad058a4b31bc637aef208f4e
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2025-05-07 22:00:27 +0300

    13.12

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
     attributes: { exclude: ['userId']},
     include: {
       model: User,
-      attributes: ['name']
+      attributes: ['id', 'name','username']
     }
   })
   res.json(blogs)
